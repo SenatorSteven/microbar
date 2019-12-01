4570f8f5ea290cd28612489c0028e10b34618f12
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-01 11:17:30 +0200

    Add files via upload

---
+++ headers/eventLoop.h
@@ -0,0 +1,7 @@
+#ifndef EVENTLOOP_H
+#define EVENTLOOP_H
+
+void eventLoop(Display *const display, const Window *const topLevelWindow, const char *const pathArray, unsigned int *const mode);
+
+#endif
+
