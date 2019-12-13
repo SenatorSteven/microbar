b1766c438bad49239e9660135b70220c7d3a2504
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-13 19:44:48 +0200

    Update defines.h

--- headers/defines.h
+++ headers/defines.h
@@ -1,8 +1,8 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define ProgramName "microbar"
-#define DefaultCharactersCount /*------------*/ ((unsigned int)200)
-#define DefaultLinesCount /*-----------------*/ ((unsigned int)100)
+#define ProgramName /*------------*/ "microbar"
+#define DefaultCharactersCount /*-*/ ((unsigned int)200)
+#define DefaultLinesCount /*------*/ ((unsigned int)100)
 
 #endif
