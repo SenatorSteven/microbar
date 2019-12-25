8ccf1602716947ba27ce85a99f4f46039cbda02c
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-25 22:19:15 +0200

    Update eventLoop.h

--- headers/eventLoop.h
+++ headers/eventLoop.h
@@ -1,6 +1,6 @@
 #ifndef EVENTLOOP_H
 #define EVENTLOOP_H
 
-void eventLoop(Display *const display, const char *const pathArray, const Window *const topLevelWindowArray, const unsigned int *const monitorAmount, unsigned int *const mode);
+void eventLoop(const char *const pathArray, const Window *const topLevelWindowArray, const unsigned int *const monitorAmount, unsigned int *const mode);
 
 #endif
