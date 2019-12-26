fec8ec42f48aba0c4ccbbb5a6d8e3b76563ecc78
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-26 10:09:28 +0200

    Update eventLoop.h

--- headers/eventLoop.h
+++ headers/eventLoop.h
@@ -1,6 +1,6 @@
 #ifndef EVENTLOOP_H
 #define EVENTLOOP_H
 
-void eventLoop(const Window *const topLevelWindowArray, const unsigned int *const monitorAmount);
+void eventLoop(const Window *const topLevelWindowArray);
 
 #endif
