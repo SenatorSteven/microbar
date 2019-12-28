d51914fa3aed6b11e92c43a0c96c2e829a761ffe
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-28 11:53:45 +0200

    Update readConfig.h

--- headers/readConfig.h
+++ headers/readConfig.h
@@ -3,6 +3,7 @@
 
 #include "defines.h"
 
+unsigned int readConfigScan(const Window *const parentWindow);
 unsigned int readConfigTopLevelWindow(const Window *const parentWindow, int *const x, int *const y, unsigned int *const width, unsigned int *const height, unsigned int *const border, bytes4 *const borderColor, bytes4 *const backgroundColor, bytes4 *const globalMenuBorderColor, bytes4 *const globalMenuBackgroundColor, unsigned int *const menuAmount);
 unsigned int readConfigMenuWindow(const Window *const parentWindow, const unsigned int *const currentMenu, int *const x, int *const y, unsigned int *const width, unsigned int *const height, unsigned int *const border, bytes4 *const borderColor, bytes4 *const backgroundColor, bytes4 *const globalBoxBorderColor, bytes4 *const globalBoxBackgroundColor, unsigned int *const boxAmount);
 unsigned int readConfigBoxWindow(const Window *const parentWindow, const unsigned int *const currentMenu, const unsigned int *const currentBox, int *const x, int *const y, unsigned int *const width, unsigned int *const height, unsigned int *const border, bytes4 *const borderColor, bytes4 *const backgroundColor, unsigned int *const innerBoxAmount);
