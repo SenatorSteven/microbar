0f98b9f5d09af5c07f4494fa751f2a34d198f928
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2019-12-31 12:42:45 +0200

    Update getParameters.h

--- headers/getParameters.h
+++ headers/getParameters.h
@@ -1,6 +1,6 @@
 #ifndef GETPARAMETERS_H
 #define GETPARAMETERS_H
 
-bool getParameters(const unsigned int *const argumentCount, const char *const *const argumentVector);
+bool getParameters(const unsigned int *const restrict argumentCount, const char *const restrict *const restrict argumentVector);
 
 #endif
