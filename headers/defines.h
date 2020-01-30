2a51bf5e0126ac3e4611496b9b8ed1a48352d362
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2020-01-30 23:04:04 +0200

    Update defines.h

--- headers/defines.h
+++ headers/defines.h
@@ -9,10 +9,10 @@
 typedef int bytes4;
 /* # /user-defined constants end # # # # */
 
-typedef unsigned int bool;
-
 #define ModeContinue /*-----------*/ ((unsigned int)0)
 #define ModeRestart /*------------*/ ((unsigned int)1)
 #define ModeExit /*---------------*/ ((unsigned int)2)
+
+typedef unsigned int bool;
 
 #endif
