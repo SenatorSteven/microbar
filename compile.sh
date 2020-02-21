9286f4f830d5eb8ce4b98901e61afb214d71fca3
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2020-02-21 10:12:27 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -27,5 +27,5 @@
 if [ ! -d "output" ]; then
 	mkdir output
 fi
-gcc -Wall -Wextra -pedantic globals.c microbar.c getParameters.c readConfig.c eventLoop.c -lX11 -lXrandr -o output/microbar
+gcc -Wall -Wextra -pedantic -std=c99 globals.c microbar.c getParameters.c readConfig.c eventLoop.c -lX11 -lXrandr -o output/microbar
 exit 0
