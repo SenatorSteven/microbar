ef0a35590ebc169241f35961609f6ab98cf8b79c
AryanGoddess <36710396+TheSenatorSteven@users.noreply.github.com>
2020-02-24 23:07:23 +0200

    Update getParameters.c

--- getParameters.c
+++ getParameters.c
@@ -23,7 +23,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <dirent.h>
 #include "headers/defines.h"
 
