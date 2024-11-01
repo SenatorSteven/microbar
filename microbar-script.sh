8465d81a23cd99b3de8bec6b097dfa83209ab539
Steven Tsakiris <36710396+SenatorSteven@users.noreply.github.com>
2024-11-01 13:10:31 +0200

    Update microbar-script.sh

--- microbar-script.sh
+++ microbar-script.sh
@@ -1,24 +1,17 @@
 
-# if the minor separator is used in awk with some \"\" trick it will not work as intented all the time. prime example: the \.
-
-
-
-
-
-
-
-
-
 #!/bin/bash
 
 # separators
-	majorSeparator=" | "
+	majorSeparator="  "
 	minorSeparator=" / "
 
+# globals
+	upower=$(upower -e)
+
 # workspaces
-	root=$(xprop -root)
-	currentWorkspace=$(($(echo "$root" | grep _NET_CURRENT_DESKTOP\(CARDINAL\) | cut -d ' ' -f 3)+1))
-	workspaces=$(echo "$root" | grep _NET_DESKTOP_NAMES\(UTF8_STRING\) | cut -d ' ' -f3- | awk "{gsub(/[,\"]/, \"\"); for(i=1; i<=NF; ++i) if(i==$currentWorkspace) printf \"[%s]\", \$i; else printf \" %s \", \$i}")
+	r=$(xprop -root)
+	currentWorkspace=$(echo "$r" | grep _NET_CURRENT_DESKTOP\(CARDINAL\) | cut -d ' ' -f 3)
+	workspaces=$(echo "$r" | grep _NET_DESKTOP_NAMES\(UTF8_STRING\) | cut -d ' ' -f 3- | awk "{gsub(/[,\"]/, \"\"); for(i=1; i<=NF; ++i) if(i==$((currentWorkspace+1))) printf \"[%s]\", \$i; else printf \" %s \", \$i}")
 	if [ "$workspaces" == '' ]; then
 		workspaces=none
 	fi
@@ -52,47 +45,59 @@
 		fi
 	done
 	wifi=none
+	ethernet=none
 	if [ "$wifiName" != '' ]; then
 		wifiIP=$(echo $addresses | grep -oP "$wifiName.*inet \K[^ ]+")
 		wifiStatus=$(echo "$links" | grep -oP "$wifiName.*state \K[^ ]+")
-		wifiStatus=${wifiStatus,,}
+		wifi=${wifiStatus,,}
 		if [ "$wifiIP" != '' ]; then
-			wifi="$wifiStatus$minorSeparator$wifiIP"
-		else
-			wifi=$wifiStatus
+			wifi="$wifi$minorSeparator$wifiIP"
 		fi
 	fi
-	ethernet=none
 	if [ "$ethernetName" != '' ]; then
 		ethernetIP=$(echo $addresses | grep -oP "$ethernetName.*inet \K[^ ]+")
 		ethernetStatus=$(echo "$links" | grep -oP "$ethernetName.*state \K[^ ]+")
-		ethernetStatus=${ethernetStatus,,}
+		ethernet=${ethernetStatus,,}
 		if [ "$ethernetIP" != '' ]; then
-			ethernet="$ethernetStatus$minorSeparator$ethernetIP"
-		else
-			ethernet=$ethernetStatus
+			ethernet="$ethernet$minorSeparator$ethernetIP"
 		fi
 	fi
 
 # bluetooth
-	bluetooth=$(upower -i $(upower -e | grep headphones | head -n 1) | grep percentage | awk '{print $2}')
+	bluetooth=$(upower -i $(echo "$upower" | grep headphones | head -n 1) | grep percentage | awk '{print $2}')
 	if [ "$bluetooth" == '' ]; then
 		bluetooth=none
 	fi
 
-# volume
-	volume=$(pactl get-sink-volume $(pactl get-default-sink))
-	secondVolume=''
-	if [ "$(echo $volume | awk "{print \$12}")" != '' ]; then
-		secondVolume="\"$minorSeparator\" \$12"
+# input
+	input=$(pactl get-source-volume @DEFAULT_SOURCE@)
+	mute=$(pactl get-source-mute @DEFAULT_SOURCE@ | cut -d ' ' -f 2)
+	whichInputs='5'
+	if [ "$(echo $input | cut -d ' ' -f 12)" != '' ]; then
+		whichInputs='5,12'
 	fi
-	volume=$(echo $volume | awk "{print \$5 $secondVolume}")
+	input=$(echo $input | cut -d ' ' -f $whichInputs --output-delimiter="$minorSeparator")
+	if [ "$mute" != no ]; then
+		input="$input (muted)"
+	fi
+
+# volume
+	volume=$(pactl get-sink-volume @DEFAULT_SINK@)
+	mute=$(pactl get-sink-mute @DEFAULT_SINK@ | cut -d ' ' -f 2)
+	whichVolumes='5'
+	if [ "$(echo $volume | cut -d ' ' -f 12)" != '' ]; then
+		whichVolumes='5,12'
+	fi
+	volume=$(echo $volume | cut -d ' ' -f $whichVolumes --output-delimiter="$minorSeparator")
+	if [ "$mute" != no ]; then
+		volume="$volume (muted)"
+	fi
 
 # load
-	load=$(cat /proc/loadavg | awk '{print $1}')
+	load=$(cat /proc/loadavg | cut -d ' ' -f 1)
 
 # battery
-	battery=$(upower -i $(upower -e | grep battery | head -n 1) | awk '/present|state|time to full|time to empty|percentage/')
+	battery=$(upower -i $(echo "$upower" | grep battery | head -n 1) | awk '/present|state|time to full|time to empty|percentage/')
 	present=$(echo "$battery" | grep present | awk '{print $2}')
 	if [ "$present" == no ]; then
 		battery=none
@@ -110,23 +115,40 @@
 	fi
 
 # date and time
-	case $(date +%d) in
-		1 | 21 | 31) dayOrdinal=st;;
-		2 | 22     ) dayOrdinal=nd;;
-		3 | 23     ) dayOrdinal=rd;;
-		*          ) dayOrdinal=th;;
-	esac
-	dateTime=$(date "+%A, %d$dayOrdinal of %B %Y$majorSeparator%H:%M:%S")
+	case $(date +%-d) in 1|21|31)o=st;;2|22)o=nd;;3|23)o=rd;;*)o=th;;esac
+	dateTime=$(date "+%A, %-d$o of %B %Y, %H:%M:%S")
 
-# result
-	printf "%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s" \
-		"$workspaces" \
-		"Wi-Fi: $wifi" \
-		"Ethernet: $ethernet" \
-		"Bluetooth: $bluetooth" \
-		"Volume: $volume" \
-		"Load: $load" \
-		"Battery: $battery" \
-		"$dateTime"
+# result 1
+	section1=$(printf "%s" "$workspaces")
+	section2=$(printf "%s" "$dateTime")
+	section3=$(printf "%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s" "Wi-Fi: $wifi" "Ethernet: $ethernet" "Bluetooth: $bluetooth" "Volume: $volume" "Load: $load" "Battery: $battery")
+
+	section1Width=$(echo "$section1" | wc -c)
+	section2Width=$(echo "$section2" | wc -c)
+	section3Width=$(echo "$section3" | wc -c)
+
+	barCharacterWidth=323
+	spacing1Width=$((($barCharacterWidth - $section2Width) / 2 - $section1Width))
+	spacing2Width=$(($barCharacterWidth - $section3Width - $section1Width - $section2Width - $spacing1Width))
+	extraWidth=$(($barCharacterWidth - $section1Width - $spacing1Width - $section2Width - $spacing2Width - $section3Width))
+	if (( spacing1Width < 0 )); then spacing1Width=0; fi
+	if (( spacing2Width < 0 )); then spacing2Width=0; fi
+	if ((    extraWidth < 0 )); then    extraWidth=0; fi
+	spacing1=$(printf "%*s" $(($spacing1Width - $extraWidth)) ' ')
+	spacing2=$(printf "%*s" $spacing2Width ' ')
+
+	printf "%s%s%s%s%s" "$section1" "$spacing1" "$section2" "$spacing2" "$section3"
+
+# result 2
+	#printf "%s [%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s$majorSeparator%s]" \
+	#	"$workspaces" \
+	#	"Wi-Fi: $wifi" \
+	#	"Ethernet: $ethernet" \
+	#	"Bluetooth: $bluetooth" \
+	#	"Input: $input" \
+	#	"Volume: $volume" \
+	#	"Load: $load" \
+	#	"Battery: $battery" \
+	#	"$dateTime"
 
 exit 0
